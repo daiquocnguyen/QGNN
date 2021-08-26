72f76fdf284cbf022c2e456d03c8dfd47faa5127
Dai Quoc Nguyen <dai.nguyen@monash.edu>
2021-08-27 00:48:06 +1000

    Release a Pytorch implementation of Gated GNNs

--- q4gnn.py
+++ q4gnn.py
@@ -23,9 +23,9 @@ def make_quaternion_mul(kernel):
 
 """Gated Quaternion GNNs"""
 class GatedQGNN(nn.Module):
-    def __init__(self, feature_dim_size, hidden_size, num_GNN_layers, num_classes, dropout, act=nn.functional.relu):
+    def __init__(self, feature_dim_size, hidden_size, num_classes, dropout, num_steps=1, act=nn.functional.relu):
         super(GatedQGNN, self).__init__()
-        self.num_GNN_layers = num_GNN_layers
+        self.num_steps = num_steps
         self.dropout_encode = nn.Dropout(dropout)
         self.emb_encode = Parameter(torch.FloatTensor(feature_dim_size//4, hidden_size))
         self.z0 = Parameter(torch.FloatTensor(hidden_size//4, hidden_size))
@@ -68,7 +68,7 @@ class GatedQGNN(nn.Module):
         x = inputs
         x = self.dropout_encode(x)
         x = torch.matmul(x, make_quaternion_mul(self.emb_encode))
-        for idx_layer in range(self.num_GNN_layers):
+        for idx_layer in range(self.num_steps):
             x = self.gatedGNN(x, adj)
         return x
     
